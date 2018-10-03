7c19545394e35f7f420df3e6add9ed9197a7a26c
Charles McQuain <charlesmcquain@gmail.com>
2018-10-03 16:27:10 -0700

    Created int, byte, short, and long variables and gave values

--- src/com/cmcquain/Main.java
+++ src/com/cmcquain/Main.java
@@ -3,6 +3,10 @@ package com.cmcquain;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myValue = 10000;
+
+	    byte myByteValue = -128;
+	    short myShortValue = 32767;
+	    long myLongValue = 9_223_372_036_854_775_807L;
     }
 }
