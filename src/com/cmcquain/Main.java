ae683b932a4ab8e6e605a181a6835ddd8743041b
Charles McQuain <charlesmcquain@gmail.com>
2018-10-03 17:10:33 -0700

    Printed myLong value and ran test

--- src/com/cmcquain/Main.java
+++ src/com/cmcquain/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	    byte myByte = 24;
 	    short myShort = 1;
 	    int myInt = 100000;
-	    long myLong = (long) (50000 * 10 + myByte) + (myShort + myInt);
+	    long myLong = (long) 50000L + 10L * (myByte + myShort + myInt);
+	    System.out.println("myLong = " + myLong);
     }
 }
