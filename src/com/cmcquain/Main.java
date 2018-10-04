41606fa0cbfba303ef0b8a3c3ad67e7c914e274a
Charles McQuain <charlesmcquain@gmail.com>
2018-10-03 17:08:50 -0700

    Completed exercise to create values of each type

--- src/com/cmcquain/Main.java
+++ src/com/cmcquain/Main.java
@@ -3,10 +3,9 @@ package com.cmcquain;
 public class Main {
 
     public static void main(String[] args) {
-	    int myValue = 10000;
-
-	    byte myByteValue = -128;
-	    short myShortValue = 32767;
-	    long myLongValue = 9_223_372_036_854_775_807L;
+	    byte myByte = 24;
+	    short myShort = 1;
+	    int myInt = 100000;
+	    long myLong = (long) (50000 * 10 + myByte) + (myShort + myInt);
     }
 }
